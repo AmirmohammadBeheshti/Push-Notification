021f53bbe192e9a2dce4825d285439b45b7bc0d8
AmirmohammadBeheshti <bamir9707@gmail.com>
2023-07-14 15:39:20 +0330

    send notification functionality

--- src/fcm/fcm.module.ts
+++ src/fcm/fcm.module.ts
@@ -1,9 +1,8 @@
 import { Module } from '@nestjs/common';
 import { FcmService } from './fcm.service';
-import { FcmController } from './fcm.controller';
 
 @Module({
   providers: [FcmService],
-  controllers: [FcmController]
+  exports: [FcmService],
 })
 export class FcmModule {}
--- src/fcm/fcm.service.ts
+++ src/fcm/fcm.service.ts
@@ -1,4 +1,77 @@
-import { Injectable } from '@nestjs/common';
-
+import { Injectable, Logger } from '@nestjs/common';
+import * as firebaseAdmin from 'firebase-admin';
+import * as path from 'path';
 @Injectable()
-export class FcmService {}
+export class FcmService {
+  private readonly logger = new Logger(FcmService.name);
+  constructor() {
+    if (firebaseAdmin.apps.length === 0) {
+      firebaseAdmin.initializeApp({
+        credential: firebaseAdmin.credential.cert(
+          path.join(__dirname, '..', '..', 'firebase-admin-token.json'),
+        ),
+      });
+    }
+  }
+
+  private readonly options = {
+    priority: 'high',
+    timeToLive: 60 * 60 * 24,
+  };
+
+  private readonly optionsSilent = {
+    priority: 'high',
+    timeToLive: 60 * 60 * 24,
+    content_available: true,
+  };
+
+  async sendNotification(
+    deviceIds: Array<string>,
+    payload: firebaseAdmin.messaging.MessagingPayload,
+    silent: boolean,
+  ) {
+    if (deviceIds.length == 0) {
+      throw new Error('You provide an empty device ids list!');
+    }
+
+    let result = null;
+    try {
+      result = await firebaseAdmin
+        .messaging()
+        .sendToDevice(
+          deviceIds,
+          payload,
+          silent ? this.optionsSilent : this.options,
+        );
+    } catch (error) {
+      this.logger.error(error.message, error.stackTrace, 'fcm-nestjs');
+      throw error;
+    }
+    return result;
+  }
+
+  async sendToTopic(
+    topic: 'all' | string,
+    payload: firebaseAdmin.messaging.MessagingPayload,
+    silent: boolean,
+  ) {
+    if (!topic && topic.trim().length === 0) {
+      throw new Error('You provide an empty topic name!');
+    }
+
+    let result = null;
+    try {
+      result = await firebaseAdmin
+        .messaging()
+        .sendToTopic(
+          topic,
+          payload,
+          silent ? this.optionsSilent : this.options,
+        );
+    } catch (error) {
+      this.logger.error(error.message, error.stackTrace, 'fcm-nestjs');
+      throw error;
+    }
+    return result;
+  }
+}
